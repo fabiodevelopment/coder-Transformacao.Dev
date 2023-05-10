7d5667ec04d72dfffd07e7cffe9d9ff092693e63
fabiodevelopment <fabiohpaes@gmail.com>
2023-05-09 23:37:13 -0300

    Aula 2 - Transformação Dev - App Fundamentos - Criação da Stack fundamentos

---
+++ react-next/fundamentos/src/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-black text-white text-6xl
+
+    `}>
+      <h1>Fundamentos</h1>
+    </div>
+  )
+}
