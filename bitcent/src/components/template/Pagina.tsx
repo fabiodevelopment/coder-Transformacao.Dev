db16509231bb923dccf1841c0bd9bc9f190f6ae6
fabiodevelopment <fabiohpaes@gmail.com>
2023-05-14 10:37:10 -0300

    Aula 4.2 - Transformação Dev - App BitCent - Implementação dos compomentes de Formulario, dados Mockados

--- bitcent/src/components/template/Pagina.tsx
+++ bitcent/src/components/template/Pagina.tsx
@@ -9,7 +9,11 @@ export default function Pagina(props: PaginaProps) {
     return (
         <div className={`
             flex flex-col min-h-screen
-            bg-gradient-to-b from-violet-950 to-slate-950
+            ${props.externa ? (
+                'bg-gradient-to-b from-violet-950 to-slate-950'
+            ) : (
+                'bg-neutral-950'
+            )}
         `}>
             {props.children}
         </div>
--- bitcent/src/pages/usuario.tsx
+++ bitcent/src/pages/usuario.tsx
@@ -1,15 +1,24 @@
 import Cabecalho from "@/components/template/Cabecalho";
+import Conteudo from "@/components/template/Conteudo";
 import Pagina from "@/components/template/Pagina";
+import TituloPagina from "@/components/template/TituloPagina";
 import Id from "@/logic/core/comum/Id";
-
+import { IconForms } from "@tabler/icons-react";
+import usuario from "@/data/constants/usuarioFalso";
+import Formularios from "@/components/usuario/Formularios";
 
 export default function CadastroUsuario() {
     return (
         <Pagina>
             <Cabecalho />
-            <div>
-                Usuário!!! {Id.novo()}
-            </div>
+            <Conteudo>
+                <TituloPagina
+                    icone={<IconForms />}
+                    principal="Dados Cadastrais"
+                    secundario={`Informações de ${usuario.email}`}
+                />
+                <Formularios />
+            </Conteudo>
         </Pagina>
     )
 }
